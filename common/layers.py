657b452293e55d723ee8b4c0f73af6bc66d528f5
noissefnoc <noissefnoc@gmail.com>
2018-08-07 16:59:29 +0900

    finish implementing 1.3.4

---
+++ common/layers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        # ensure deep copy
+        self.grads[0][...] = dW
+        return dx
