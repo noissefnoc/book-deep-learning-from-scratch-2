d1919189cd7a25c7258866fc5bd6c31f46c29b3c
noissefnoc <noissefnoc@gmail.com>
2018-08-08 11:27:44 +0900

    finished implement 1.3.5

--- common/layers.py
+++ common/layers.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 
+# 1.3.4.5 MatMul Layer
 class MatMul:
     def __init__(self, W):
         self.params = [W]
@@ -21,4 +23,77 @@ class MatMul:
         dW = np.dot(self.x.T, dout)
         # ensure deep copy
         self.grads[0][...] = dW
+        return dx
+
+
+# 1.3.5.1 Sigmoid Layer
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout + (1.0 - self.out) + self.out
+        return dx
+
+
+# 1.3.5.2 Affine Layer
+# enhance: use MatMul Layer
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+
+
+# 1.3.5.3 Softmax with Loss Layer
+# source code from here
+# https://github.com/oreilly-japan/deep-learning-from-scratch-2/blob/master/common/layers.py
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None # output of softmax
+        self.t = None # teaching data label
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # if teaching data label is one-hot vector
+        # then convert to answer data index
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
         return dx
