843712ec7559ee6edd0d7d2d06db2845dca71552
noissefnoc <noissefnoc@gmail.com>
2018-08-08 14:02:17 +0900

    bug fix: finished implement 1.4.4

--- ch01/train_custom_loop.py
+++ ch01/train_custom_loop.py
@@ -51,3 +51,30 @@ if __name__ == '__main__':
                       % (epoch + 1, iters + 1, max_iters, avg_loss))
                 loss_list.append(avg_loss)
                 total_loss, loss_count = 0, 0
+
+    # plot learning result
+    plt.plot(np.arange(len(loss_list)), loss_list, label='train')
+    plt.xlabel('iterations (x10)')
+    plt.ylabel('loss')
+    plt.show()
+
+    # plot boundary
+    h = 0.001
+    x_min, x_max = x[:, 0].min() - .1, x[:, 0].max() + .1
+    y_min, y_max = x[:, 1].min() - .1, x[:, 1].max() + .1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    X = np.c_[xx.ravel(), yy.ravel()]
+    score = model.predict(X)
+    predict_cls = np.argmax(score, axis=1)
+    Z = predict_cls.reshape(xx.shape)
+    plt.contourf(xx, yy, Z)
+    plt.axis('off')
+
+    # plot data points
+    x, t = spiral.load_data()
+    N = 100
+    CLS_NUM = 3
+    markers = ['o', 'x', '^']
+    for i in range(CLS_NUM):
+        plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+    plt.show()
