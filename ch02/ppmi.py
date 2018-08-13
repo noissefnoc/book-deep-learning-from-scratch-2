aa6715372b411fc3675b73b7bbe1dcc3461d8bc4
noissefnoc <noissefnoc@gmail.com>
2018-08-13 23:29:46 +0900

    finished implement 2.4.1

---
+++ ch02/ppmi.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import numpy as np
+from common.util import preprocess, create_co_matrix, cos_similarity, ppmi
+
+
+if __name__ == '__main__':
+    text = 'You say goodbye and I say hello.'
+    corpus, word_to_id, id_to_word = preprocess(text)
+    vocab_size = len(word_to_id)
+    C = create_co_matrix(corpus, vocab_size)
+    W = ppmi(C)
+
+    np.set_printoptions(precision=3)
+    print('covariance matrix')
+    print(C)
+    print('-' * 50)
+    print('PPMI')
+    print(W)
--- common/util.py
+++ common/util.py
@@ -105,3 +105,24 @@ def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
 
         if count >= top:
             return
+
+
+# 2.4.1 Pointwise Mutual Information (PMI)
+def ppmi(C, verbose=False, eps=1e-8):
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j] * S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total // 100) == 0:
+                    print('%.1f%% done' % (100 * cnt / total))
+
+    return M
