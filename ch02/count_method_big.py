bb7b9b7ef225c9f9ceeb6e35d026781748e15800
noissefnoc <noissefnoc@gmail.com>
2018-08-14 16:33:53 +0900

    finished implement 2.4.5

---
+++ ch02/count_method_big.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import numpy as np
+from common.util import most_similar, create_co_matrix, ppmi
+from dataset import ptb
+
+
+if __name__ == '__main__':
+    window_size = 2
+    wordvec_size = 100
+
+    corpus, word_to_id, id_to_word = ptb.load_data('train')
+    vocab_size = len(word_to_id)
+    print('counting co-occurrence ...')
+    C = create_co_matrix(corpus, vocab_size, window_size)
+    print('calculating PPMI ...')
+    W = ppmi(C, verbose=True)
+
+    print('calculating SVD ...')
+    try:
+        # truncated SVD (fast!)
+        from sklearn.utils.extmath import randomized_svd
+        U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5, random_state=None)
+    except ImportError:
+        # SVD (slow)
+        U, S, V = np.linalg.svd(W)
+
+    word_vecs = U[:, :wordvec_size]
+
+    queries = ['you', 'year', 'car', 'toyota']
+
+    for query in queries:
+        most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
