3ebfc547a87a05d3d9421d16e0bbb263dd33b9b4
noissefnoc <noissefnoc@gmail.com>
2018-08-15 09:56:36 +0900

    finished implement 4.1.2

--- common/layers.py
+++ common/layers.py
@@ -97,3 +97,26 @@ class SoftmaxWithLoss:
         dx = dx / batch_size
 
         return dx
+
+
+# 4.1.2 Embedding Layer
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+
+        return None
