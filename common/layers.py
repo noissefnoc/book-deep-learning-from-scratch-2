d54c8dd68bf336f8b6a3a4180c9809060782cd4c
noissefnoc <noissefnoc@gmail.com>
2018-08-15 11:03:18 +0900

    implement SigmoidWithLoss for 4.2.7

--- common/layers.py
+++ common/layers.py
@@ -120,3 +120,26 @@ class Embedding:
             dW[word_id] += dout[i]
 
         return None
+
+
+# 4.2.7 SigmoidWithLoss
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None # sigmoid output
+        self.t = None # teaching data
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
