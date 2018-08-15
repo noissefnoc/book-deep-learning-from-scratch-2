71f604d8ad74b1d376a424c105f3ac8d738c0efb
noissefnoc <noissefnoc@gmail.com>
2018-08-15 11:11:19 +0900

    finished implement 4.2.7

--- ch04/negative_sampling_layer.py
+++ ch04/negative_sampling_layer.py
@@ -2,7 +2,7 @@
 
 import collections
 import numpy as np
-from common.layers import Embedding
+from common.layers import Embedding, SigmoidWithLoss
 
 
 # 4.2.4 multi-classification to binary classification
@@ -71,3 +71,45 @@ class UnigramSampler:
                 p=p)
 
         return negative_sample
+
+
+# 4.2.7 Negative Sampling implement
+class NegativeSamplingLoss:
+    def __init__(self, W, corpus, power=0.75, sample_size=5):
+        self.sample_size = sample_size
+        self.sampler = UnigramSampler(corpus, power, sample_size)
+        self.loss_layers = [SigmoidWithLoss() for _ in range(sample_size + 1)]
+        self.embed_dot_layers = [EmbeddingDot(W) for _ in range(sample_size + 1)]
+        self.params, self.grads = [], []
+
+        for layer in self.embed_dot_layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+    def forward(self, h, target):
+        batch_size = target.shape[0]
+        negative_sample = self.sampler.get_negative_sample(target)
+
+        # positive example forward
+        score = self.embed_dot_layers[0].forward(h, target)
+        correct_label = np.ones(batch_size, dtype=np.int32)
+        loss = self.loss_layers[0].forward(score, correct_label)
+
+        # negative example forward
+        negative_label = np.zeros(batch_size, dtype=np.int32)
+
+        for i in range(self.sample_size):
+            negative_target = negative_sample[:, i]
+            score = self.embed_dot_layers[1 + i].forward(h, negative_target)
+            loss += self.loss_layers[1 + i].forward(score, negative_label)
+
+        return loss
+
+    def backward(self, dout=1):
+        dh = 0
+
+        for l0, l1 in zip(self.loss_layers, self.embed_dot_layers):
+            dscore = l0.backward(dout)
+            dh += l1.backward(dscore)
+
+        return dh
