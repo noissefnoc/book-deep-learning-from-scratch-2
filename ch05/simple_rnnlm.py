40e6d9cebdbbe1605619855323a266224f87f116
noissefnoc <noissefnoc@gmail.com>
2018-08-16 22:11:57 +0900

    fix bug

--- ch05/simple_rnnlm.py
+++ ch05/simple_rnnlm.py
@@ -11,8 +11,8 @@ class SimpleRnnlm:
 
         # initialize weights
         embed_W = (rn(V, D) / 100).astype('f')
-        rnn_Wx = (rn(D, H) / np.sort(D)).astype('f')
-        rnn_Wh = (rn(H, H) / np.sort(H)).astype('f')
+        rnn_Wx = (rn(D, H) / np.sqrt(D)).astype('f')
+        rnn_Wh = (rn(H, H) / np.sqrt(H)).astype('f')
         rnn_b = np.zeros(H).astype('f')
         affine_W = (rn(H, V) / np.sqrt(H)).astype('f')
         affine_b = np.zeros(V).astype('f')
